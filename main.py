0268cf8e8d9232a3e329788a8bb8831a8b81acd4
Pratham Soni <psoni2002@gmail.com>
2018-01-07 17:40:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ import os
 flags = tf.app.flags
 flags.DEFINE_integer("epoch", 100, "Number of epoch [100]")
 flags.DEFINE_integer("batch_size", 128, "The size of batch images [128]")
-flags.DEFINE_integer("image_size", 42, "The size of image to use [63]")
-flags.DEFINE_integer("label_size", 21, "The size of label to produce [21]")
+flags.DEFINE_integer("image_size", 33, "The size of image to use [33]")
+flags.DEFINE_integer("label_size", 17, "The size of label to produce [17]")
 flags.DEFINE_float("learning_rate", 1e-4, "The learning rate of gradient descent algorithm [1e-4]")
 flags.DEFINE_float("momentum",0.9,"The momentum of SGD [0.9]")###add momentum for better training performance
 flags.DEFINE_integer("c_dim", 1, "Dimension of image color. [1]")
 flags.DEFINE_integer("scale", 3, "The size of scale factor for preprocessing input image [3]")
-flags.DEFINE_integer("stride", 42, "The size of stride to apply input image [14]")
+flags.DEFINE_integer("stride", 14, "The size of stride to apply input image [14]")
 flags.DEFINE_string("checkpoint_dir", "checkpoint", "Name of checkpoint directory [checkpoint]")
 flags.DEFINE_string("sample_dir", "sample", "Name of sample directory [sample]")
 flags.DEFINE_boolean("is_train", True, "True for training, False for testing [True]")
